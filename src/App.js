ccba02209c617ba6b54dbbcce2d828ebe0b50387
Catherine Aguilera <catherineaguilera30@gmail.com>
2022-07-06 16:47:42 -0400

    add main components and assets

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import {Box} from '@mui/material';
 import ExerciseDetail from './pages/ExerciseDetail';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 import "./App.css";
 
 const App = () => {
   return (
-    <Box width="400px">
+    <Box width="400px" sx={{width: {xl: '1488px'}}} m="auto">
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/exercise/:id" element={<ExerciseDetail/>}/>
       </Routes>
+      <Footer />
     </Box>
   )
 }
